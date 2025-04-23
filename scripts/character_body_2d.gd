ac738bdcab518a9623bd709853baa5cf34bfccdf
Anda Elena Sucitu <andaelena05@gmail.com>
2025-04-23 20:52:28 +0300

    character fixed

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,28 +1,19 @@
 extends CharacterBody2D
 
-
 const SPEED = 400.0
 const JUMP_VELOCITY = -800.0
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
-	# Animations
-	
-	if (velocity.x > 1 || velocity.x < -1):
-		sprite_2d.animation = "running"
-	else:
-		sprite_2d.animation = "default"
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		sprite_2d.animation = "jumping"
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -30,6 +21,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, 12)
 
 	move_and_slide()
-	
+
+	# Flip sprite
 	var isLeft = velocity.x < 0
 	sprite_2d.flip_h = isLeft
+
+	# Animations (prioritate: jumping > running > idle)
+	if not is_on_floor():
+		sprite_2d.animation = "jumping"
+	elif abs(velocity.x) > 1:
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
