1a5435992eadf05c90086c5ca69079df215a073e
ancaraduu <anca.radu2411@stud.acs.upb.ro>
2025-05-27 00:24:51 +0300

    working on the slimezzz

--- scripts/winter_scripts/blue_slime.gd
+++ scripts/winter_scripts/blue_slime.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 60
+const GRAVITY := 1200.0
+const JUMP_FORCE := -450.0
+const JUMP_INTERVAL := 1.5
+
 var direction = 1
 var stunned := false  # blocăm mișcarea în timpul damage
+var jump_timer := 0.0
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
@@ -13,6 +17,30 @@ var current_health := max_health
 
 func _ready():
 	add_to_group("enemy")
+	jump_timer = JUMP_INTERVAL
+
+func _physics_process(delta: float) -> void:
+	if stunned:
+		return
+
+	# Gravitație
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	else:
+		jump_timer -= delta
+		if jump_timer <= 0:
+			jump()
+			jump_timer = JUMP_INTERVAL
+
+	# doar pe jos și nu în timpul unei alte animații
+	if not blue_slime.is_playing() or blue_slime.animation != "jump":
+		blue_slime.play("default")
+		
+	move_and_slide()
+
+func jump():
+	velocity.y = JUMP_FORCE
+	blue_slime.play("jump")
 
 func take_damage():
 	if stunned:
@@ -34,7 +62,7 @@ func take_damage():
 
 	if current_health <= 0:
 		die()
-		
+
 	blue_slime.play("default")
 
 func die():
