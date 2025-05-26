c88f0c92b4e2b56eb7226d76e3daed50fd14a7ba
ancaraduu <anca.radu2411@stud.acs.upb.ro>
2025-05-26 17:33:56 +0300

    Completed Key/Chest logic

--- scripts/winter_scripts/winter_game_manager.gd
+++ scripts/winter_scripts/winter_game_manager.gd
@@ -2,4 +2,6 @@ extends Node
 
 func _on_key_collected():
 	print("Afiseaza!!")
-	get_node("../CollectedKey/Control/Sprite2D").visible = true  # Afișăm iconița
+	# Afisam iconita -> cheia a fost preluata
+	var icon_key = get_node("../CollectedKey/Control/Sprite2D")
+	icon_key.visible = true
--- scripts/winter_scripts/winter_key.gd
+++ scripts/winter_scripts/winter_key.gd
@@ -1,9 +1,24 @@
 extends Area2D
 
 signal key_collected  # Semnal pe care îl trimitem la colectare
+
+@onready var Sparkle = $Sparkle
 	
 func _on_body_entered(body):
 	if body.name == "CharacterBody2D":  # Numele personajului
-		print("Cheie!")
-		emit_signal("key_collected")    # Trimite semnal
-		queue_free()                    # Șterge cheia din scenă
+		# Dezactivează coliziunea imediat, ca să nu se retrigeze
+		$CollisionShape2D.disabled = true
+
+		# Pornește animatia și ascunde cheia
+		$AnimatedSprite2D.visible = false  # ascunde cheia vizual
+		emit_signal("key_collected")
+		get_node("../WinterChest").has_key = true
+		
+		Sparkle.visible = true
+		Sparkle.play("sparkle")
+		
+		# Așteaptă un frame ca să fii sigur ca animatia incepe
+		await get_tree().process_frame
+		# Așteaptă finalul animatiei
+		await Sparkle.animation_finished
+		queue_free()
